3ee74af93c4b2d012e78ac4b1b028f56d6b9e3ae
Santiago Salgado <jssalgadog@gmail.com>
2022-07-04 11:57:14 -0500

    models_tweets

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Python
-from datetime import date
+from datetime import date, datetime
 from uuid import UUID
 from typing import Optional
 
@@ -21,7 +21,8 @@ class UserBase(BaseModel):
 class UserLogin(UserBase):
     password: str = Field(
         ...,
-        min_length=8
+        min_length=8,
+        max_length=64
     )
 
 class User(UserBase): 
@@ -38,7 +39,15 @@ class User(UserBase):
     birth_date: Optional[date] = Field(default=None)
 
 class Tweet():
-    pass
+    tweet_id = UUID = Field(...)
+    content = str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 @app.get(
     path="/",
