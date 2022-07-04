708eb5d80632f4d559f5ea03d20da05eda7a06d0
Santiago Salgado <jssalgadog@gmail.com>
2022-07-04 17:40:31 -0500

    users_show_all

--- main.py
+++ main.py
@@ -114,7 +114,24 @@ def login():
     tags=["Users"]
 )
 def show_all_users():
-    pass
+    """
+    Show Users
+
+    This path operation shows all users in the app
+
+    Parameters:
+        -
+    
+    Returns a json list with all users in the app, with the following keys
+        - user_ir: UUID
+        - email: Emailstr
+        - first_name: str
+        - last_name: str
+        - birth_date: date
+    """
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Show user by Id
 @app.get(
